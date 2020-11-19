5296b54c6f8b52939ef457c3e59698ac5babd42e
DriesDD <driesdedecker@gmail.com>
2020-11-19 15:18:23 +0100

    move to svelte

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+
+<script>
+  export let name;
+</script>
+
+<style global>
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+  @tailwind utilities;
+</style>
+
+<main class="py-4 px-10">
+  <h1>Hello {name}!</h1>
+  <p class="p-2">
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
